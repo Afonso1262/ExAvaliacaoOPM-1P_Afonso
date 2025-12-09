d199117ef573b5ad9fb58afd23dd55174530740b
Afonso1262 <1262@iai.pt>
2025-12-09 11:29:53 +0000

    jjk

--- Ex-5/app.js
+++ Ex-5/app.js
@@ -10,29 +10,31 @@ console.log("nº sorte: " + numeroSorte, "nº visitante: " + adivinhar)
 
 let ganhou = false
 
-if (adivinhar > 10 || adivinhar <= 0) {
-    alert('Esse número não é válido, recomeça.')
-} else {
-    while (!ganhou) {
-        if (numeroSorte === adivinhar) {
-            alert('Certo!')
-            ganhou = true
+for (let adivinhar = 0; adivinhar < 6; adivinhar++) {
+    if (adivinhar > 10 || adivinhar <= 0) {
+        alert('Esse número não é válido, recomeça.')
+    } else {
+        while (!ganhou) {
+            if (numeroSorte === adivinhar) {
+                alert('Certo!')
+                ganhou = true
 
-        } else if (numeroSorte > adivinhar && numeroSorte - adivinhar <= 2 || numeroSorte < adivinhar && adivinhar - numeroSorte <= 2) {
-            alert('Quase lá!')
+            } else if (numeroSorte > adivinhar && numeroSorte - adivinhar <= 2 || numeroSorte < adivinhar && adivinhar - numeroSorte <= 2) {
+                alert('Quase lá!')
 
-            adivinhar = prompt('Adivinha o número da sorte entre 1 e 10')
-            adivinhar = Math.floor(Number(adivinhar))
-        } else if (numeroSorte > adivinhar) {
-            alert('Muito Abaixo')
+                adivinhar = prompt('Adivinha o número da sorte entre 1 e 10')
+                adivinhar = Math.floor(Number(adivinhar))
+            } else if (numeroSorte > adivinhar) {
+                alert('Muito Abaixo')
 
-            adivinhar = prompt('Adivinha o número da sorte entre 1 e 10')
-            adivinhar = Math.floor(Number(adivinhar))
-        } else {
-            alert('Muito Acima!')
+                adivinhar = prompt('Adivinha o número da sorte entre 1 e 10')
+                adivinhar = Math.floor(Number(adivinhar))
+            } else {
+                alert('Muito Acima!')
 
-            adivinhar = prompt('Adivinha o número da sorte entre 1 e 10')
-            adivinhar = Math.floor(Number(adivinhar))
+                adivinhar = prompt('Adivinha o número da sorte entre 1 e 10')
+                adivinhar = Math.floor(Number(adivinhar))
+            }
         }
     }
 }
